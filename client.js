2ae3f69e29d44266202fe759ee6c957841afed9f
Kevin Cogen <k.cogen@gmail.com>
2023-02-26 15:42:59 -0500

    lots of callback expirements commented out

--- client.js
+++ client.js
@@ -5,20 +5,54 @@ const connect = function() {
     host: "localhost",
     port: "50541"
   });
-
-  // interpret incoming data as test
-  conn.setEncoding("utf-8");
-
+  conn.setEncoding("utf-8")  // interpret incoming data as te
   conn.on("connect", () => {
-    console.log("Successfully connected to game server")
+    console.log("Successfully connected to game server");
+  })
+  conn.on("connect", () => {
+    conn.write("Name: KMC");
   })
   conn.on("data", (data) => {
-    console.log(data)
+    console.log(data) //recieve from server
   })
-  conn.on("connect", () => {
-    conn.write("Name: KMC")
-  })
+
+  //Same Callback
+  // conn.on("connect", () => {
+  //   console.log("Successfully connected to game server"); //confirm connect
+  // }, () => {
+  //   conn.write("Name: KMC"); //send intials to game (needs to be delayed if part of the same connect callback?)
+  // }, () => {
+  //   conn.write("Move: up") //move
+  // })
+
+
+  //different callbacks
+
+  // conn.on("connect", () => {
+  //   console.log("Successfully connected to game server");
+  // })
+  // conn.on("connect", () => {
+  //   conn.write("Name: KMC");
+  // })
+  // conn.on("connect", () => {
+  //   conn.write("Move: up")
+  // })
   return conn;
 };
 
 module.exports = {connect};
+
+// how to delay the callback
+// setTimeout(() => {
+//   conn.write("Move: up") //move
+// }, 50))
+
+//Same Callback - with timeout
+// conn.on("connect", () => {
+//   console.log("Successfully connected to game server"); //confirm connect
+// }, setTimeout(() => {
+//   conn.write("Name: KMC"); //send intials to game (needs to be delayed if part of the same connect callback?)
+// }, 5), setTimeout(() => {
+//   conn.write("Move: up") //move
+// }, 1000))
+
