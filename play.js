5297332a8bbfe0a631a1b167cf79bc398929f708
Kevin Cogen <k.cogen@gmail.com>
2023-02-26 17:53:20 -0500

    stdin setup

--- play.js
+++ play.js
@@ -3,5 +3,8 @@ const net = require("net");
 // establishes a connection with the game server
 const {connect} = require("./client");
 
+const {setupInput} = require("./client")
+
 console.log("Connecting ...");
 connect();
+setupInput();
