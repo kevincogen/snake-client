696349aa0d8cd74cf04af9858b42d11536b23358
Kevin Cogen <k.cogen@gmail.com>
2023-02-26 10:58:32 -0500

    updated on.connect function and on.data function. Writes KMC to game server

--- client.js
+++ client.js
@@ -3,17 +3,20 @@ const net = require("net");
 const connect = function() {
   const conn = net.createConnection({
     host: "localhost",
-    port: 50541
+    port: "50541"
   });
 
   // interpret incoming data as test
   conn.setEncoding("utf-8");
 
   conn.on("connect", () => {
-    console.log("connected!")
+    console.log("Successfully connected to game server")
   })
   conn.on("data", (data) => {
     console.log(data)
+  })
+  conn.on("connect", () => {
+    conn.write("Name: KMC")
   })
   return conn;
 };
