933c183928418519c8a5009ce880baf663762b5e
Kevin Cogen <k.cogen@gmail.com>
2023-02-27 23:02:58 -0500

    input.js for submission

--- input.js
+++ input.js
@@ -5,35 +5,24 @@ const setupInput = function(conn) {
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
-
   return stdin;
 };
-
+const inputKey = function(key, expect, write) { //helper for handleUserInput
+  if (key === expect) {
+    connection.write(write);
+  }
+};
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
-    connection.write('Move: up');
-  }
-  if (key ==='a') {
-    connection.write('Move: left');
-  }
-  if (key === 's') {
-    connection.write('Move: down');
-  }
-  if (key === 'd') {
-    connection.write('Move: right'); //connection.log printed like console.log?
-  }
-  if (key === 'f') {
-    connection.write("Say: uh oh");
-  }
-  if (key === 'g') {
-    connection.write("Say: Good Game");
-  }
-  if (key === 'h') {
-    connection.write("Say: Holy Smokes Batman!");
-  }
+  inputKey(key, 'w', 'Move: up');
+  inputKey(key, 'a', 'Move: left');
+  inputKey(key, 's', 'Move: down');
+  inputKey(key, 'd', 'Move: right');
+  inputKey(key, 'f', 'Say: uh oh');
+  inputKey(key, 'g', 'Say: Good Game');
+  inputKey(key, 'h', 'Say: Holy Smokes Batman!');
 };
 let connection;
 module.exports = {setupInput};
